689f47cd02ac2a4512e3da31d0fd21fecf047bf4
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2023-02-08 08:54:46 +0100

    '#'

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
 
-};
+    string = string.replace(/[./ /,!]/g, "");
 
+    const reversed = string.split('').reverse().join('');
+
+    if (string === reversed) {
+        return true
+    } else { return false }
+
+    console.log(string, reversed)
+
+
+}
 // Do not edit below this line
 module.exports = palindromes;
